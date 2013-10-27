21b57900afe6cc0b74bc39799e0c62c8d32012ed
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2013-10-27 13:12:03 +0800

    for a change

--- my_daily_gist/interface/views.py
+++ my_daily_gist/interface/views.py
@@ -61,22 +61,22 @@ def json_test(request):
     ret = [
         {
             "id":"1",
-            "longitude":"13.939",
-            "latitude":"121.155",
+            "longitude":"13.939115",
+            "latitude":"121.155161",
             "description":"This is where i am living",
             "name":"My House"
         },
         {
             "id":"2",
-            "longitude":"13.938",
-            "latitude":"121.153",
+            "longitude":"13.938758",
+            "latitude":"121.153868",
             "description":"Gasoline station on our street",
             "name":"Phoenix"
         },
         {
             "id":"3",
-            "longitude":"13.938",
-            "latitude":"121.154",
+            "longitude":"13.938053",
+            "latitude":"121.154968",
             "description":"Kaberks house",
             "name":"Regie"
         }
