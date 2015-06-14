ff287cf400f631429803b77bda236baec5966d6f
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2015-06-15 04:28:09 +0800

    fix pymongo error

--- my_daily_gist/interface/util.py
+++ my_daily_gist/interface/util.py
@@ -6,8 +6,9 @@ def insert_gists(data_dict):
     mongo_client = pymongo.MongoClient(settings.MONGODB_HOST, settings.MONGODB_PORT)
     mongo_db = mongo_client[settings.MONGODB_NAME]
     mongo_collection = mongo_db[settings.MONGODB_COLLECTION_NAME]
+    mongo_collection.delete_many({})
 
-    mongo_collection.update_many({}, data_dict, upsert=True)
+    mongo_collection.insert_many([d_d for d_d in data_dict ])
 
 def load_gists():
 
@@ -15,4 +16,4 @@ def load_gists():
     mongo_db = mongo_client[settings.MONGODB_NAME]
     mongo_collection = mongo_db[settings.MONGODB_COLLECTION_NAME]
 
-    return [gist for gist in mongo_collection.find()]
+    return [gist for gist in mongo_collection.find({})]
