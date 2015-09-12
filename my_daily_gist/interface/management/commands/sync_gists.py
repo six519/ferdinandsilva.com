47094ac9c202e7181a206b8bb3a1053deea1a2f5
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2015-09-12 12:06:56 +0800

    save gists in db

--- my_daily_gist/interface/management/commands/sync_gists.py
+++ my_daily_gist/interface/management/commands/sync_gists.py
@@ -1,7 +1,24 @@
 from django.core.management.base import BaseCommand, CommandError
+from interface.models import *
+import requests
 
 class Command(BaseCommand):
     help = 'Syncs github gists'
 
     def handle(self, *args, **options):
-        pass
+        print "Fetching gists..."
+        gists = requests.get('https://api.github.com/users/six519/gists').json()
+
+        cnt = 0
+        for i, gist in enumerate(gists):
+            print "Reading gist # %s" % (i + 1)
+            gst = SocialPost.objects.filter(post_id=gist["id"])
+
+            if gst.count() == 0:
+                cnt += 1
+                new_gist = SocialPost()
+                new_gist.description = gist["description"]
+                new_gist.post_id = gist["id"]
+                new_gist.save()
+
+        print "Number of new gists saved: %s" % cnt
