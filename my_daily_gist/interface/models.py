e14bc71b30c476f2462dfc0058f10b682b29beec
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2015-09-12 10:42:54 +0800

    add posts model

--- my_daily_gist/interface/models.py
+++ my_daily_gist/interface/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class SocialPost(models.Model):
+
+    SOCIAL_POST_GIST = 0
+    SOCIAL_POST_YOUTUBE = 1
+
+    class Meta:
+        db_table = "social_posts"
+
+    description = models.CharField(db_column="description", max_length=255, blank=True)
+    post_id = models.CharField(db_column="post_id", max_length=255, blank=True)
+    post_type = models.PositiveIntegerField(db_column="post_type", default=SOCIAL_POST_GIST)
