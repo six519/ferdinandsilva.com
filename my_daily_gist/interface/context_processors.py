bae7a6fe8ff520107f202276a61166822517c49f
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2017-11-27 09:49:14 +0800

    implement image post

--- my_daily_gist/interface/context_processors.py
+++ my_daily_gist/interface/context_processors.py
@@ -4,5 +4,6 @@ def mysite_contexts(request):
 
 	return {
 		"SOCIAL_POST_GIST": SocialPost.SOCIAL_POST_GIST,
-		"SOCIAL_POST_YOUTUBE": SocialPost.SOCIAL_POST_YOUTUBE
+		"SOCIAL_POST_YOUTUBE": SocialPost.SOCIAL_POST_YOUTUBE,
+		"SOCIAL_POST_IMAGE": SocialPost.SOCIAL_POST_IMAGE
 	}
