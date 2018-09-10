a7e73b5ebfed814c8f9ac4ce98d96d28c9eb7e81
Ferdinand Silva <ferdinandsilva@ferdinandsilva.com>
2018-09-10 12:45:26 +0800

    change to titik

--- my_daily_gist/grab_gists.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-
-import requests
-
-gists = requests.get('https://api.github.com/users/six519/gists')
-
-for gist in gists.json():
-	print "%s: %s" % (gist['id'], gist['description'])
--- my_daily_gist/interface/context_processors.py
+++
@@ -1,9 +0,0 @@
-from interface.models import SocialPost
-
-def mysite_contexts(request):
-
-	return {
-		"SOCIAL_POST_GIST": SocialPost.SOCIAL_POST_GIST,
-		"SOCIAL_POST_YOUTUBE": SocialPost.SOCIAL_POST_YOUTUBE,
-		"SOCIAL_POST_IMAGE": SocialPost.SOCIAL_POST_IMAGE
-	}
